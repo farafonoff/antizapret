9369d8f8064ee775a6d58987cf21bdb802dc1967
Artem Farafonov <farafonoff@gmail.com>
2019-09-14 19:23:11 +0500

    added ipv6 filter, because iptables part doesnt support ipv6 filtering for now

--- update_blocks3.sh
+++ update_blocks3.sh
@@ -4,10 +4,6 @@ echo $MY_PATH
 pushd $MY_PATH
 FILE=rkn3.set
 ISET=rkn_new
-#wget "http://www.vpngate.net/api/iphone/" -O vpns.csv
-#wget "https://api.antizapret.info/group.php" -O groups
-#wget -4 "http://antizapret.prostovpn.org/proxy.pac" -O /tmp/rkn.pac
-#egrep -o "([0-9]+\.){3}[0-9]+" /tmp/rkn.pac > /tmp/blackips_pac
 echo update submodules..
 git submodule update --recursive --remote
 echo "create $ISET hash:net family inet hashsize 2048 maxelem 100000000" > $FILE
@@ -16,10 +12,9 @@ IFS="|"
 cut -d";" -f1 z-i/dump.csv | tail -n +2 | while read blockline
 do
 	for ip in $blockline; do
-		echo "add $ISET $ip" >> $FILE
+		echo "add $ISET $ip"
 	done;
-done 
-
+done | egrep -v '[0-9]:' >> $FILE
 echo "add $ISET 91.198.22.70" >> $FILE
 
 echo set generated $ISET
--- vpnd.py
+++ vpnd.py
@@ -3,76 +3,94 @@ import csv
 import random
 import base64
 import threading
-import httplib2
-from subprocess import Popen,PIPE
+# import httplib2
+import http.client
+from subprocess import Popen, PIPE
 import time
+import traceback
 
 censors = ['RU', 'KR']
-httpcli = httplib2.Http(".cache")
+# httpcli = httplib2.Http(".cache")
+
 
 def dump_out(pp):
-	while pp.returncode==None:
-		ps = pp.stdout.readline()
-		ps = ps.strip()
-		if len(ps)>0:
-			print(ps)
-		pp.poll()
-	print(pp.returncode)
+    while pp.returncode == None:
+        ps = pp.stdout.readline()
+        ps = ps.strip()
+        if len(ps) > 0:
+            print(ps)
+        pp.poll()
+    print(pp.returncode)
+
 
 def runvpn(config):
-	cf = open('/tmp/vpn.cfg', 'wb');
-	cf.write(config)
-	cf.close()
-	pp = Popen(['/usr/sbin/openvpn', '--config', '/tmp/vpn.cfg', '--dev', 'tun101', '--script-security', '2','--route-noexec' , '--up', 'unblock_ovpn.sh','--down', 'downvpn.sh'], stdout=PIPE)
-	bgthread = threading.Thread(target=dump_out, args=(pp,))
-	bgthread.start()
-	time.sleep(40)
-	while testvpn():
-		print('PING OK')
-		time.sleep(20)
-	try:
-		print('PING FAILED')
-		pp.terminate()
-	except:
-		print ('already dead')
-	
+    cf = open('/tmp/vpn.cfg', 'wb')
+    cf.write(config)
+    cf.close()
+    pp = Popen(
+        ['/usr/sbin/openvpn', '--config', '/tmp/vpn.cfg', '--dev', 'tun101', '--script-security',
+         '2', '--route-noexec', '--up', 'unblock_ovpn.sh', '--down', 'downvpn.sh'], stdout=PIPE)
+    bgthread = threading.Thread(target=dump_out, args=(pp,))
+    bgthread.start()
+    time.sleep(40)
+    while testvpn():
+        print('PING OK')
+        time.sleep(20)
+    try:
+        print('PING FAILED')
+        pp.terminate()
+    except:
+        print ('already dead')
+
+
 def testvpn():
-	pp = Popen(['ping', '8.8.8.8', '-I', 'tun101', '-w', '3'])
-	pp.wait()
-	if pp.returncode==0:
-		return True
-	else:
-		return False
+    pp = Popen(['ping', '8.8.8.8', '-I', 'tun101', '-w', '3'])
+    pp.wait()
+    if pp.returncode == 0:
+        return True
+    else:
+        return False
+
+
 def newservlist():
-    src = 'http://www.vpngate.net/api/iphone/'
-    (resp, content) = httpcli.request(src)
+    # src = 'http://www.vpngate.net/api/iphone/'
+    host = 'www.vpngate.net'
+    path = '/api/iphone/'
+    conn = http.client.HTTPConnection(host, 80, timeout=10)
+    conn.request("GET", path)
+    response = conn.getresponse()
+    print(response.status, response.reason)
+    content = response.read()
+    content = content
+    #(resp, content) = httpcli.request(src)
     lines = content.splitlines()
     return map(str, lines)
 
+
 def pickAndConnect(serversList):
-	reader = csv.reader(serversList, delimiter=',')
-	slist = [];
-	for row in reader:
-		if len(row)>14 and row[2].isdigit():
-			slist.append(row)
-		else:
-			print(row)
-	count=len(slist)
-	slist = filter(lambda srv: not (srv[6] in censors), slist)
-	slist = sorted(slist, key=lambda srv: srv[2])
-	print('total items: ' + len(slist))
-	shortlist = slist[0:int(count/2)]
-	server = random.choice(shortlist)
-	print(server[:14])
-	config = base64.b64decode(server[14])
-	runvpn(config)
+    reader = csv.reader(serversList, delimiter=',')
+    slist = []
+    for row in reader:
+        if len(row) > 14 and row[2].isdigit():
+            slist.append(row)
+        else:
+            print(row)
+    count = len(slist)
+    slist = filter(lambda srv: not (srv[6] in censors), slist)
+    slist = sorted(slist, key=lambda srv: srv[2])
+    print('total items: ', len(slist))
+    server = random.choice(slist)
+    #print(server[:14])
+    config = base64.b64decode(server[14])
+    runvpn(config)
 
 while True:
     try:
-	print('GET SERVER LIST...')
-    	lines = newservlist()
-    	pickAndConnect(lines)
-    except:
-	print('FAIL')
-	time.sleep(10)
-
+        print('GET SERVER LIST...')
+        lines = newservlist()
+        pickAndConnect(lines)
+    except Exception as e:
+        print('FAIL')
+        traceback.print_exc()
+        print(e)
+        time.sleep(10)
